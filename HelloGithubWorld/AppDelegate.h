21dc361dfe1ab88056f2475d167c6321f17e8bf3
Michael Yum <michael.yum@gmail.com>
2018-03-26 23:16:56 -0700

    Added some comments

---
+++ HelloGithubWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloGithubWorld
+//
+//  Created by Michael Yum on 3/26/18.
+//  Copyright © 2018 Michael Yum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloGithubWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloGithubWorld
+//
+//  Created by Michael Yum on 3/26/18.
+//  Copyright © 2018 Michael Yum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
